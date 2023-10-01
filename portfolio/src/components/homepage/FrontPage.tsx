171c8cf39c3783e9925e0af64b26037effa9e346
moshawwa <mohammedshawa10@gmail.com>
2023-10-01 23:56:26 +0300

    edit img path in public directory

--- portfolio/src/components/homepage/FrontPage.tsx
+++ portfolio/src/components/homepage/FrontPage.tsx
@@ -1,16 +1,24 @@
 const FrontPage = () => {
   return (
     <>
-      <div className="container" style={{ marginTop: "145px" }}>
+      <div className="container" style={{ marginTop: "135px" }}>
         <div className="row">
-          <div className="col-8">
+          <div className="col-7" style={{ marginTop: "135px" }}>
             <h2>Hi I am Mohammed Al-Shawwa</h2>
-            <p>Software Engineer react.js developer</p>
+            <h4>Software Engineer react.js developer</h4>
+            <p>
+              I strongly believe my knowledge is my destiny. i have always loved
+              trying things out for myself. Some people find that science is
+              just a way to get a good and comfortable job. in my opinion.
+              science is just about curiosity to find out more. and never to
+              stop
+            </p>
+            <button className="btn btn-outline-danger">Download Resume</button>
           </div>
-          <div className="col-4">
+          <div className="col-5">
             <img
-              style={{ width: "100%", height: "100%", borderRadius: "9999px" }}
-              src="https://drive.google.com/file/d/1suXdxs-ow94EGSgYHZ0Y5yK-vEE6A5dS/view?usp=share_link"
+              style={{ width: "100%", height: "100%", borderRadius: "50%" }}
+              src="/profile.jpeg"
             />
           </div>
         </div>
