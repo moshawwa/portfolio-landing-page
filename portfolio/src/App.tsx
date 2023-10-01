c255c2a5bdd87c81f7b5493d83c5e9aa9c5f0d45
moshawwa <mohammedshawa10@gmail.com>
2023-10-02 00:01:55 +0300

    remove comment in app.tsx

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
-import FeaturdWork from "./components/homepage/FeaturdWork";
+
 import FrontPage from "./components/homepage/FrontPage";
-import RecentPosts from "./components/homepage/RecentPosts";
 
 function App() {
   return (
     <>
       <FrontPage />
-      {/* <RecentPosts />
-      <FeaturdWork /> */}
     </>
   );
 }
