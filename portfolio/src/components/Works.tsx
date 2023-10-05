e73baddc5c05f646a875dcca3c1b57dcc0b865ef
moshawwa <mohammedshawa10@gmail.com>
2023-10-05 18:47:43 +0300

    add works in the works page

--- portfolio/src/components/Works.tsx
+++ portfolio/src/components/Works.tsx
@@ -1,7 +1,34 @@
 const Works = () => {
   return (
     <>
-      <h1>works</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-7">
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                style={{ padding: "15px" }}
+                src="/stack-of-books.png"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Book Gallery</h5>
+                <p className="card-text">
+                  This is a simple gallery of books that I have read and
+                  enjoyed. It was built using React, Redux, Asp.net, Express,
+                  MongoDB
+                </p>
+                <a
+                  href="https://book-gallery.vercel.app/"
+                  className="btn btn-primary"
+                >
+                  visit website
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
