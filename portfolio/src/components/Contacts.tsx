98da8779e3913a9267bb4cadc167b0c2444a6af0
moshawwa <mohammedshawa10@gmail.com>
2023-10-05 20:47:03 +0300

    contacts page generated

--- portfolio/src/components/Contacts.tsx
+++ portfolio/src/components/Contacts.tsx
@@ -1,26 +1,84 @@
+import { BsFillTelephoneFill, BsWhatsapp } from "react-icons/bs";
+import { HiOutlineMailOpen, HiLocationMarker } from "react-icons/hi";
+
 const Contacts = () => {
   return (
     <div className="container mt-4">
       <div className="row">
-        <div className="col-md-7">
+        <div className="col-md-12">
           <h2>Contact Information</h2>
-          <div className="ms-4 mt-4">
-            <p>
-              <strong style={{ color: "#FF6464" }}>Name:</strong> Mohammed Hazem
-              Rebhi AL-Shawwa
-            </p>
-            <p>
-              <strong style={{ color: "#FF6464" }}>Email:</strong>{" "}
-              mohammedshawa10@gmail.com
-            </p>
-            <p>
-              <strong style={{ color: "#FF6464" }}>Phone:</strong> (+972)
-              59-7802-445
-            </p>
-            <p>
-              <strong style={{ color: "#FF6464" }}>Address:</strong> 123 Main
-              Street, City, State, ZIP
-            </p>
+
+          <div className="row d-flex flex-wrap">
+            <div
+              className="card"
+              style={{ width: "18rem", margin: "12px", flexGrow: 1 }}
+            >
+              <div
+                className="card-icon"
+                style={{ margin: "30px", color: "#FF6464" }}
+              >
+                <HiOutlineMailOpen size={250} />
+              </div>
+
+              <div className="card-body">
+                <p className="cart-title">Email</p>
+                <p className="card-text">mohammedshawa10@gmail.com</p>
+              </div>
+            </div>
+
+            <div
+              className="card"
+              style={{ width: "18rem", margin: "12px", flexGrow: 1 }}
+            >
+              <div
+                className="card-icon"
+                style={{ margin: "30px", color: "#FF6464" }}
+              >
+                <BsFillTelephoneFill size={250} />
+              </div>
+
+              <div className="card-body">
+                <p className="cart-title">Phone Number</p>
+                <p className="card-text">+(972) 59-7802-445</p>
+              </div>
+            </div>
+
+            <div
+              className="card"
+              style={{ width: "18rem", margin: "12px", flexGrow: 1 }}
+            >
+              <div
+                className="card-icon"
+                style={{ margin: "30px", color: "#FF6464" }}
+              >
+                <HiLocationMarker size={250} />
+              </div>
+
+              <div className="card-body">
+                <p className="cart-title">Location</p>
+                <p className="card-text">
+                  Al Yarmouk st behind Alzaharnah building Gaza City -Gaza Strip
+                  Palestine
+                </p>
+              </div>
+            </div>
+
+            <div
+              className="card"
+              style={{ width: "18rem", margin: "12px", flexGrow: 1 }}
+            >
+              <div
+                className="card-icon"
+                style={{ margin: "30px", color: "#FF6464" }}
+              >
+                <BsWhatsapp size={250} />
+              </div>
+
+              <div className="card-body">
+                <p className="cart-title">whatsapp number</p>
+                <p className="card-text">+(972) 56-7802445</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
