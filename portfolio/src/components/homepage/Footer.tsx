8e6893ec6519a136335a44e7318e5950f3e53cea
moshawwa <mohammedshawa10@gmail.com>
2025-07-09 15:31:38 +0300

    remove twitter icon

--- portfolio/src/components/homepage/Footer.tsx
+++ portfolio/src/components/homepage/Footer.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   const socialLinks = [
